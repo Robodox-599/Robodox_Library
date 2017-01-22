5db10f9a8033638529108aa8673cb400ba310fc4
Tons-of-Carls <carlsim66@yahoo.com>
2017-01-22 13:51:41 -0800

    added pixy code to library

--- src/C++/Sensors/Pixy/Pixy.cpp
+++ src/C++/Sensors/Pixy/Pixy.cpp
@@ -1,10 +1,65 @@
 /*
  * Pixy.cpp
  *
- *  Created on: Mar 30, 2016
+ *  Created on: Jan 8, 2017
  *      Author: Admin
  */
+#include "Pixy.h"
 
+Pixy::Pixy()
+{
+	Sensor = new I2C(I2C::Port::kOnboard, 0x2);
+	buffer[12] = {};
+	sync[2] = {};
+	pixyValues[6] = {};
+}
 
+Pixy::~Pixy()
+{
+	delete Sensor;
+	Sensor = nullptr;
+}
 
+void Pixy::convert(int byteOne, int byteTwo)
+{
+	pixyValues[byteOne/2] = buffer[byteOne] | (buffer[byteTwo] << 8);
+}
 
+bool Pixy::updateBuffer()
+{
+	Sensor->Transaction(NULL, 0, sync, 1);
+	if(sync[0] == 85)
+	{
+		Sensor->Transaction(NULL, 0, sync, 1);
+		if(sync[0] == 170)
+		{
+			Sensor->Transaction(NULL, 0, sync, 1);
+			if(sync[0] == 85)
+			{
+				Sensor->Transaction(NULL, 0, sync, 1);
+				if(sync[0] == 170)
+				{
+					Sensor->Transaction(NULL, 0, buffer, 12);
+					convert(0,1);
+					convert(2,3);
+					convert(4,5);
+					convert(6,7);
+					convert(8,9);
+					convert(10,11);
+					return true;
+				}
+			}
+		}
+	}
+	else if(sync[0] == 0)
+	{
+		//no object detected
+		//place appropriate code
+	}
+	return false;
+}
+
+uint16_t Pixy::getValue(Value v)
+{
+	return pixyValues[v];
+}
--- src/C++/Sensors/Pixy/Pixy.h
+++ src/C++/Sensors/Pixy/Pixy.h
@@ -1,15 +1,46 @@
 /*
  * Pixy.h
  *
- *  Created on: Mar 28, 2016
+ *  Created on: Jan 8, 2017
  *      Author: Admin
  */
+#include "WPILib.h"
+#ifndef SRC_PIXY_H_
+#define SRC_PIXY_H_
 
-#ifndef SRC_C___SENSORS_PIXY_PIXY_H_
-#define SRC_C___SENSORS_PIXY_PIXY_H_
+class Pixy
+{
+public:
+	Pixy();
+	~Pixy();
+
+	I2C* Sensor;
+
+	enum Value
+	{
+		CHECKSUM,
+		SIGNATURE,
+		X_CENTER,
+		Y_CENTER,
+		WIDTH,
+		HEIGHT
+	};
+
+	bool updateBuffer();
+	uint16_t getValue(Value v);
+
+private:
+
+	void convert(int byteOne, int byteTwo);
+
+	uint8_t sync[2];
+	uint8_t buffer[12];
+
+	uint16_t pixyValues[6];
+
+
+};
 
 
 
-
-
-#endif /* SRC_C___SENSORS_PIXY_PIXY_H_ */
+#endif /* SRC_PIXY_H_ */
